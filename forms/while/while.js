fa66902a1e7360e1cd36d70e385f0cf8f1ecaca4
jessicafeese <jaf32246@creighton.edu>
2021-09-22 16:41:48 -0500

    finish

---
+++ forms/while/while.js
@@ -0,0 +1,8 @@
+
+let wantToContinue=true
+let i=1
+while(wantToContinue){
+  console.log(`This is loop iteration ${i}.`)
+  wantToContinue=confirm('Do you want to go again? OK for Yes, Cancel for No.')
+  i++
+}
