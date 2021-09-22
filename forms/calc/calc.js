f26cc77c896fd3a2dfc0e18da15d0118c3b601e0
jessicafeese <jaf32246@creighton.edu>
2021-09-22 16:18:02 -0500

    calc

---
+++ forms/calc/calc.js
@@ -0,0 +1,13 @@
+function Calculate(num1, num2) {
+   let answer = num1 + num2
+   return answer
+}
+
+let number1 = prompt('Enter your first number: ')
+let number2 = prompt('Enter your second number: ')
+
+number1 = parseInt(number1)
+number2 = parseInt(number2)
+
+let newAnswer = Calculate(number1, number2)
+console.log(`The sum of ${number1} and ${number2} is ${newAnswer}`)
---
+++ forms/for/for.js
@@ -0,0 +1,6 @@
+/*
+var names = ["Bob", "Janet", "Tom", "Erin", "Lucy", "Alyssa", "Dustin"]
+
+for (i = 0; i< names.length; i++)
+    console.log(`${names[i]}`)
+*/
